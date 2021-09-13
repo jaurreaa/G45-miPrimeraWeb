1049d4530cd1bb7e174fc65f14397f0ecd1e4141
jaurrea <jau2705@gmail.com>
2021-09-13 14:38:47 -0500

    terminado html

---
+++ clase_g18/js/interaccion.js
@@ -0,0 +1,15 @@
+console.log('Hola Mundo!');
+
+var num1 = 3;
+var num2 = 5;
+
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+
+var i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
